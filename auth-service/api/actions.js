43e46ed9f89a6d80bfad978e31f405fb0907f08c
khaue.viana <khaue.viana@concrete.com.br>
2018-01-11 16:48:59 -0200

    Reactor auth

--- auth-service/api/actions.js
+++ auth-service/api/actions.js
@@ -7,29 +7,27 @@ const axios = require("axios");
 const jwt = require('jsonwebtoken');
 
 const factory = () => ({
-  signin: (request, response, next) => {
+  signin: async (request, response, next) => {
 
-    new Promise((resolve, reject) => {
+    const data = { username: request.body.username, password: request.body.password }
 
-      const data = { username: request.body.username, password: request.body.password }
+    const validator = Joi.validate(data, schema);
 
-      const result = Joi.validate(data, schema);
+    if (validator.error) {
+      next(validator.error);
+    } else {
+      const result = await axios.get(`${env.app.userService}?username=${data.username}&password=${data.password}`);
 
-      if (result.error) {
-        reject(result.error);
+      if (result.data) {
+        response.status(200).json(jwt.sign({
+          email: result.data.email,
+          username: result.data.username,
+          id: result.data._id
+        }, 'RESTFULAPIs'));
       } else {
-        resolve(data);
+        response.status(400).json("Invalid Login or Password.")
       }
-      
-    }).then(result => axios.get(`${env.app.userService}?username=${result.username}&password=${result.password}`))
-      .then(result => {
-        if (result.data) {
-          response.status(200).json(jwt.sign({ email: result.data.email, username: result.data.username, id: result.data._id }, 'RESTFULAPIs'));
-        } else {
-          response.status(400).json("Invalid Login or Password.")
-        }
-      })
-      .catch(next)
+    }
   }
 })
 
