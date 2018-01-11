137530068cbbedef43e6600137801221fd5d89d4
khaue.viana <khaue.viana@concrete.com.br>
2018-01-11 16:24:26 -0200

    Add Auth

--- auth-service/api/actions.js
+++ auth-service/api/actions.js
@@ -1,8 +1,37 @@
 'use strict'
 
+const env = require('../helpers/env')
+const schema = require('./model').schema
+const Joi = require('joi');
+const axios = require("axios");
+const jwt = require('jsonwebtoken');
+
 const factory = () => ({
   signin: (request, response, next) => {
 
+    const data = {
+      username: request.body.username,
+      password: request.body.password
+    }
+
+    const result = Joi.validate(data, schema);
+
+    if (result.error) {
+      next(result.error);
+    } else {
+
+      axios.get(`${env.app.userService}?username=${data.username}&password=${data.password}`)
+        .then(result => {
+
+          if (result.data) {
+            response.status(200).json(jwt.sign({ email: result.data.email, username: result.data.username, id: result.data._id }, 'RESTFULAPIs'));
+          } else {
+            response.status(400).json("Invalid Login or Password.")
+          }
+
+        })
+        .catch(next)
+    }
   }
 })
 
--- auth-service/helpers/env.js
+++ auth-service/helpers/env.js
@@ -5,6 +5,7 @@ const dotenv = require('dotenv').config({path: './config/.env'});
 module.exports = {
   name: process.env.NODE_ENV,
   app: {
-    port: process.env.PORT
-  }  
+    port: process.env.PORT,
+    userService: process.env.USER_SERVICE
+  }
 }
