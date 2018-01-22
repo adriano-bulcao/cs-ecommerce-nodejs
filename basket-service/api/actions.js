e65b151d5894d3662f23a33c2a77bb12e7473b7b
guilherme.sampaio <guilherme.sampaio@concrete.com.br>
2018-01-22 18:17:58 -0200

    Refactoring actions from Basket API

--- basket-service/api/actions.js
+++ basket-service/api/actions.js
@@ -8,19 +8,6 @@ const env = require('../helpers/env');
 
 
 const factory = ({ repository }) => ({
-    getById: async (request, response, next) => {
-        try {
-            const basket = await repository.getById(request.params.id);
-            if (!basket) {
-                response.status(404).json({ message: "Basket not found!" });
-                return;
-            }
-
-            response.status(200).json(basket);
-        } catch (error) {
-            next(error);
-        }
-    },
     create: async (request, response, next) => {
         try {
             const result = await repository.create();
@@ -31,60 +18,40 @@ const factory = ({ repository }) => ({
         }
     },
 
+    getById: async (request, response, next) => {
+        try {
+            const basket = request.basket;
+            response.status(200).json(basket);
+        } catch (error) {
+            next(error);
+        }
+    },
+
+    //TODO's: 
+    // 1. Check on stock for product availability
+    // 2. Decrement product stock quantity when adding product to basket.
     addItem: async (request, response, next) => {
         try {
-            const basketId = request.params.id;
+            const basket = request.basket;
             const item = request.body;
-
-            //TODO: Perform here a query on Stock.API 
-            // to verify if the product is available!!!
-
-            const basket = await repository.getById(basketId);
-            if (!basket) {
-                response.status(404).json({ message: "Basket not found!" });
-                return;
-            }
-
-            if(!basket.items)
-                basket.items = [];
-
-            if (basket.items.some(it => it.productId == item.productId)) {
-                response.status(400).json({ message: `The product '${item.productId}' is already added!` });
-                return;
-            }
-
             basket.items.push(item);
-            
             const result = await repository.update(basket);
-            
+
             response.status(200).json(basket);
+
         } catch (error) {
             next(error);
         }
 
     },
-    updateQuantity: async (request, response, next) => {
 
+    updateQuantity: async (request, response, next) => {
         try {
-            const basketId = request.params.basketid;
             const itemId = request.params.itemid;
             const quantity = request.body.quantity;
-            const basket = await repository.getById(basketId);
-
-            if (!basket) {
-                response.status(404).json({ message: "Basket not found!" });
-                return;
-            }
-
-            const item = basket.items.find(it => it.productId == itemId);
-
-            if (!item) {
-                response.status(400).json({ message: `The item ${itemId} was not found!` });
-                return;
-            }
-
+            const basket = request.basket;         
+            const item = basket.items.find(it => it.productId == itemId);            
             item.quantity = quantity;
-
             const result = await repository.update(basket);
 
             response.status(200).json(basket);
@@ -92,31 +59,16 @@ const factory = ({ repository }) => ({
         } catch (error) {
             next(error);
         }
-
     },
+
     removeItem: async (request, response, next) => {
         try {
-            const basketId = request.params.basketid;
+            const basket = request.basket; 
             const itemId = request.params.itemid;
-            const quantity = request.body.quantity;
-            const basket = await repository.getById(basketId);
-
-            if (!basket) {
-                response.status(404).json({ message: "Basket not found!" });
-                return;
-            }
-
             const item = basket.items.find(it => it.productId == itemId);
-
-            if (!item) {
-                response.status(400).json({ message: `The item ${itemId} was not found!` });
-                return;
-            }
-
             const index = basket.items.indexOf(item);
-
             basket.items.splice(index, 1);
-
+            
             var result = repository.update(basket);
 
             response.status(200).json(basket);
@@ -125,33 +77,65 @@ const factory = ({ repository }) => ({
             next(error);
         }
     },
+
     checkout: async (request, response, next) => {
         try {
-            const basketId = request.params.id;
-            const basket = await repository.getById(basketId);
+            const basket = request.basket;
             const customerId = request.body.customerId;
-
-            if (!basket) {
-                response.status(404).json({ message: "Basket not found!" });
-                return;
-            }
-
             const url = `${env.external.ordersAPI}/orders`;
-
             var data = {
                 customerId,
-                items : basket.items,
-                total : basket.total
+                items: basket.items,
+                total: basket.total
             }
-
             const result = await axios.post(url, data);
-
-            response.status(200).json({message: "OK"});
-
+            response.status(200).json({ message: "OK" });
         } catch (error) {
             next(error);
         }
     },
+
+    basketVerification: async (request, response, next) => {
+        const basket = await repository.getById(request.params.basketid);
+        if (!basket) {
+            response.status(404).json({ message: "Basket not found!" });
+            return;
+        }
+        request.basket = basket;
+        next();
+    },
+
+    itemAlreadyAddedVerification: async (request, response, next) => {
+        const basket = request.basket;
+        const productId = request.body.productId;
+
+        if (!basket.items)
+            basket.items = [];
+
+        request.basket = basket;
+        
+        const basketItem = basket.items.find(it => it.productId == productId);
+        
+        if (basketItem) {
+            response.status(400).json({ message: `The product ${productId} is already added to basket!` });
+            return;
+        }
+        
+        next();
+    },
+
+    itemExistenceVerification: async (request, response, next) => {
+        const basket = request.basket;
+        const itemId = request.params.itemid;
+        const item = basket.items.find(it => it.productId == itemId);
+
+        if (!item) {
+            response.status(404).json({ message: `The item ${itemId} was not found!` });
+            return;
+        }
+
+        next();
+    }
 });
 
 exports.factory = factory;
--- basket-service/api/index.js
+++ basket-service/api/index.js
@@ -4,9 +4,9 @@ const { Router } = require('express');
 const { actions } = require('./actions');
 
 module.exports = Router()
-    .get('/basket/:id', actions.getById)
     .post('/basket', actions.create)
-    .put('/basket/:id/item', actions.addItem)
-    .put('/basket/:basketid/item/:itemid', actions.updateQuantity)
-    .delete('/basket/:basketid/item/:itemid', actions.removeItem)
-    .post('/basket/:id/checkout', actions.checkout)
+    .get('/basket/:basketid', actions.basketVerification, actions.getById)
+    .put('/basket/:basketid/item', actions.basketVerification, actions.itemAlreadyAddedVerification, actions.addItem)
+    .put('/basket/:basketid/item/:itemid', actions.basketVerification, actions.itemExistenceVerification, actions.updateQuantity)
+    .delete('/basket/:basketid/item/:itemid', actions.basketVerification, actions.itemExistenceVerification, actions.removeItem)
+    .post('/basket/:basketid/checkout', actions.basketVerification, actions.checkout)
