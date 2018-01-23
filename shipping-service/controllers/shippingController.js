03928d42ee898bd968f2acef2be51df78175509b
vitor.avelino <vitor.avelino@concretesolutions.com.br>
2018-01-23 18:10:32 -0200

    Insert

--- shipping-service/controllers/shippingController.js
+++ shipping-service/controllers/shippingController.js
@@ -1,9 +1,13 @@
 const repository = require("./../repositories/shippingRepository");
 
-exports.getById = function (req, res) {
+exports.getById = function (req, res, next) {
     repository.findOne(req.params.id).then(message => res.send(message));
 };
 
-exports.getAll = function (req, res) {
+exports.getAll = function (req, res, next) {
     repository.find().then(message => res.send(message));
+};
+
+exports.insert = function (req, res, next) {
+    repository.insert(req.body).then(res.send(200));
 };
