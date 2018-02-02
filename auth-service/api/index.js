5b63548978e9d547452a962d62e97fd018b1b8bb
celonet <marcelo.lopesdasilva@gggmail.com>
2018-02-02 11:42:50 -0200

    Removed service. will be implemented in users service

--- auth-service/api/index.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-const { Router } = require('express')
-const { actions } = require('./actions')
-
-module.exports = Router()
-  .post('/', actions.signin)
--- auth-service/index.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-
-const app = require('./app')
-const env = require('./helpers/env')
-
-app.listen(env.app.port, () => console.log(`Server up at port ${env.app.port} in ${env.name}`))
